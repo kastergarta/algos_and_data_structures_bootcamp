7a002b836d63613bd478ec2ec7beda0a37e1e4f4
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-04-22 22:45:09 -0400

    solution fith reduce still slow

--- numberZooPatrol.js
+++ numberZooPatrol.js
@@ -13,3 +13,16 @@ function findNumber(array) {
     }
 
     findNumber([1,3,4,5,6,7,8]);
+
+    function findNumber(array) {
+   
+      let min = Math.min(...array), max = Math.max(...array);
+      
+      if(array.length == 0 || min != 1) return 1;
+      
+      let sum = array.reduce((a, b) => a+b)
+      let tSum = max*(max+1)/2
+      
+      if(tSum-sum == 0) return max+1;       
+      return tSum-sum
+      }
