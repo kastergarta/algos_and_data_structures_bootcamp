3ee738bed50266a86a5492c0914212e5f9a531a3
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-09 23:38:32 -0400

    recode

--- doubleCola.js
+++ doubleCola.js
@@ -1,11 +1,4 @@
 function whoIsNext(names, r){
-//   if (names.length >= r) return names[r-1];
-//   console.log(names, r)
-//   for (i=0; i<r; i++){
-//     names.push(names[i], names[i])
-//   }
-//   return names[r]
-
 
   const iteration = Math.floor(Math.log2(((r - 1) / names.length) + 1));
   const post = 2 ** iteration;
