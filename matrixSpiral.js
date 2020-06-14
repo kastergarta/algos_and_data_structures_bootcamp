3e6669bf2199db1ca050ca513f47b6140bec9e05
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-13 20:43:31 -0400

    matrix spiral

---
+++ matrixSpiral.js
@@ -0,0 +1,42 @@
+const spiral = number => {
+	let counter = 1;
+	let startRow = 0,
+		endRow = number - 1;
+	let startColumn = 0,
+		endColumn = number - 1;
+
+	const matrix = [];
+	for (let i = 0; i < number; i++) matrix.push([]);
+
+	while (startColumn <= endColumn && startRow <= endRow) {
+		// Start Row
+		for (let i = startColumn; i <= endColumn; i++) {
+			matrix[startRow][i] = counter;
+			counter++;
+		}
+		startRow++;
+
+		// End Column
+		for (let i = startRow; i <= endRow; i++) {
+			matrix[i][endColumn] = counter;
+			counter++;
+		}
+		endColumn--;
+
+		// End Row
+		for (let i = endColumn; i >= startColumn; i--) {
+			matrix[endRow][i] = counter;
+			counter++;
+		}
+		endRow--;
+
+		// Start Column
+		for (let i = endRow; i >= startRow; i--) {
+			matrix[i][startColumn] = counter;
+			counter++;
+		}
+		startColumn++;
+	}
+
+	return matrix;
+};
