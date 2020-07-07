13860d6354b963c0a683e32d867787938631c419
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-07-07 15:05:41 -0400

    daily temperature leetcode

---
+++ dailyTemperature.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/daily-temperatures/
+
+const dailyTemperature = function(temperatures) {
+    let hash = []
+    let ans = []
+  
+    for (let i = temperatures.length; i--; ) {
+      let temperature = temperatures[i]
+      ans[i] = Infinity
+  
+      for (let j = temperature + 1; j <= 100; j++) {
+        if (hash[j] && hash[j] - i < ans[i]) {
+          ans[i] = hash[j] - i
+        }
+      }
+  
+      if (ans[i] === Infinity) ans[i] = 0
+      hash[temperature] = i
+    }
+    return ans
+  };
