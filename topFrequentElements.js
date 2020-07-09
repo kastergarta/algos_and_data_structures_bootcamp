13aab5756faad49607f6b7ecf0e4e7da26e0a916
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-07-08 22:09:22 -0400

    find k frequent element

---
+++ topFrequentElements.js
@@ -0,0 +1,25 @@
+var topKFrequent = function(nums, k) {
+    var obj = {};
+    nums.forEach(function(item) {
+      if (!obj[item])
+        obj[item] = 1;
+      else 
+        obj[item]++;
+    });
+  
+    var arr = [];
+  
+    for (var key in obj) {
+      arr.push({key: key, value: obj[key]});
+    }
+  
+    arr.sort(function(a, b) {
+      return b.value - a.value;
+    });
+  
+    var ans = [];
+    for (var i = 0; i < k; i++)
+      ans.push(+arr[i].key);
+  
+    return ans;
+  };
