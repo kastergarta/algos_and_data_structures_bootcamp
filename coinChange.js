49c53a82e7aec2cd35816b87fd35c6e573fde9b9
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-07-09 21:20:52 -0400

    coin change

---
+++ coinChange.js
@@ -0,0 +1,19 @@
+const coinChange = (coins, amount) => {
+    var ans = [];
+    ans[0] = 0;
+  
+    for (var i = 0, len = coins.length; i < len; i++) {
+      var item = coins[i];
+      for (var j = 0; j + item <= amount; j++) {
+        if (ans[j] === undefined)
+          continue;
+        if (ans[j + item] === undefined)
+          ans[j + item] = ans[j] + 1;
+        else 
+          ans[j + item] = Math.min(ans[j + item], ans[j] + 1);
+      }
+  
+    }
+  
+    return ans[amount] === undefined ? -1 : ans[amount];
+  };
