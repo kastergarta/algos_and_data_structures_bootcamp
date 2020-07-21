5ba4a78131807010171a73c6e18f795850e0fe11
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-07-21 14:22:30 -0400

    intersection of 2 arrays

---
+++ intersectionOf2Arrays.js
@@ -0,0 +1,27 @@
+var intersection = function(nums1, nums2) {
+    
+    var hash = {};
+    var result = [];
+    var i = 0;
+    while(i < nums1.length || i < nums2.length) {
+        if(i < nums1.length) {
+            hash[nums1[i]] = hash[nums1[i]] || [];
+            hash[nums1[i]][0] = true;
+        }
+        
+        if(i < nums2.length) {
+            hash[nums2[i]] = hash[nums2[i]] || [];
+            hash[nums2[i]][1] = true;
+        }
+
+        i++
+    }
+    
+    for(i in hash) {
+        if(hash[i][0] && hash[i][1]) {
+            result.push(parseInt(i));
+        }
+    }
+    
+    return result;
+};
