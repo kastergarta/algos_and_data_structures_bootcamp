dd6633b4a6810b7afdaad3d5e1e482dadca5bb4f
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-09-08 21:36:08 -0400

    * Hash Table Heep Priority Queue BFS DFS

--- mumbling.swift
+++
@@ -1,9 +0,0 @@
-func accum(_ s: String) -> String {
-  let array = Array(s)
-  var newArray:[String] = []
-  
-  for (idx, el) in array.enumerated() {
-     newArray.append(el.uppercased() + String(repeating: el, count: idx).lowercased() + "-")
-  }
-  return String(newArray.joined(separator: "").dropLast())
-}
