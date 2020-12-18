0887379b55c5960821e93e758a910ce6bd7f2cda
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-12-18 15:11:53 -0500

    rotate array

---
+++ reverseArray.js
@@ -0,0 +1,17 @@
+let rotate = function(nums, k) {
+    k = k % nums.length;
+    
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, k, nums.length - 1);
+    reverse(nums, 0, k - 1);
+};
+
+let reverse = function(nums, start, end) {
+    while (start < end) {
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }  
+};
